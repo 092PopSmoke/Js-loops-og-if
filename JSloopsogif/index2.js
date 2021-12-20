85bc16aebbb9a5d91b1e1cb16e2e07fb8714a518
Lean <93644347+092PopSmoke@users.noreply.github.com>
2021-12-20 14:30:11 +0100

    Add files via upload

---
+++ JSloopsogif/index2.js
@@ -0,0 +1,10 @@
+function difference(n) {
+    if (n <= 10)
+        return 10 - n;
+    else
+        return (n - 10) * 2;
+}
+console.log(difference(32))
+console.log(difference(11))
+
+// med hjelp av w3resource
---
+++ JSloopsogif/index4.js
@@ -0,0 +1,9 @@
+function test50(x, y) {
+    return ((x == 50 || y == 50) || (x + y == 50));
+}
+console.log(test50(50, 50))
+console.log(test50(20, 50))
+console.log(test50(20, 20))
+console.log(test50(20, 30))
+
+// med hjelp av https://www.w3resource.com/javascript-exercises/javascript-basic-exercise-18.php
